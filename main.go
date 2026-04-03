507f1bba5d35cd5e91ba3a64587614e3e0076311
deexth <hkabare@gmail.com>
2026-04-03 19:31:18 +0200

    parsing path

--- main.go
+++ main.go
@@ -6,11 +6,21 @@ import (
 )
 
 func main() {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "unable to get the user's home directory: %v", err)
+		os.Exit(1)
+	}
+
+	cfg := &Config{
+		HomeDir: homeDir,
+	}
+
 	args := os.Args
-	src, dest, err := parseArgs(args[1:])
+	src, dest, err := parseArgs(args[1:], cfg)
 	if err != nil {
 		handlerError(err)
 	}
 
-	fmt.Fprintf(os.Stdout, "source provide is %s and the destination is %s\n", src, dest)
+	fmt.Fprintf(os.Stdout, "source provided is %s and the destination is %s\n", src, dest)
 }
--- mvx.go
+++ mvx.go
@@ -4,17 +4,81 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"path/filepath"
+	"strings"
 )
 
-func parseArgs(args []string) (string, string, error) {
+type Config struct {
+	HomeDir string
+}
+
+func parseArgs(args []string, cfg *Config) (string, string, error) {
 	if len(args) < 2 {
 		return "", "", errors.New("usage: mvx <source> <destination>")
 	}
 
-	src := args[0]
-	dest := args[1]
+	for i := range args {
+		if !strings.HasPrefix(args[i], "~") {
+			continue
+		}
+
+		args[i] = strings.Split(args[i], "~")[1]
+	}
+
+	src := filepath.Clean(args[0])
+	dest := filepath.Clean(args[1])
+
+	err := cfg.parseSrc(src)
+	if err != nil {
+		return "", "", err
+	}
+
+	err = cfg.parseDest(dest)
+	if err != nil {
+		return "", "", err
+	}
 
 	return src, dest, nil
+}
+
+func (cfg *Config) parseSrc(path string) error {
+	newPath := filepath.Join(cfg.HomeDir, path)
+
+	ok := filepath.IsAbs(newPath)
+	if !ok {
+		return fmt.Errorf("no such file or directory '%s' found", path)
+	}
+	return nil
+}
+
+func (cfg *Config) parseDest(path string) error {
+	newPath := filepath.Join(cfg.HomeDir, path)
+
+	var dirPath string
+
+	ok := filepath.IsAbs(newPath)
+	if !ok {
+		err := createPath(newPath)
+		if err != nil {
+			return err
+		}
+
+		fmt.Fprintf(os.Stdout, "create path directory with parents at %s", dirPath)
+	}
+	return nil
+}
+
+func createPath(path string) error {
+	dirPath := filepath.Dir(path)
+
+	err := os.MkdirAll(dirPath, 0750)
+	if err != nil {
+		return fmt.Errorf("issue creating destination path %s: %v", dirPath, err)
+	}
+
+	// TODO: add verbose here
+
+	return nil
 }
 
 func handlerError(err error) {
